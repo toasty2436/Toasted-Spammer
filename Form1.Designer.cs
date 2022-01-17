4ad8cb1dbf6d32b84fcfab79f792f446efe27c27
Ryan Lepley <ryanlepley84@gmail.com>
2022-01-17 18:26:40 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace R6S_Chat_Spammer
+{
+    partial class Spammer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Spammer));
+            this.spamBox = new System.Windows.Forms.TextBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.HyperLink = new System.Windows.Forms.LinkLabel();
+            this.Status = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Wait = new System.Windows.Forms.Timer(this.components);
+            this.Exit = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.IntervalCounter = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.IntervalCounter)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // spamBox
+            // 
+            this.spamBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.spamBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.spamBox, "spamBox");
+            this.spamBox.ForeColor = System.Drawing.Color.Yellow;
+            this.spamBox.Name = "spamBox";
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Name = "label1";
+            // 
+            // HyperLink
+            // 
+            resources.ApplyResources(this.HyperLink, "HyperLink");
+            this.HyperLink.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.HyperLink.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.HyperLink.Name = "HyperLink";
+            this.HyperLink.TabStop = true;
+            this.HyperLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.HyperLink_LinkClicked);
+            // 
+            // Status
+            // 
+            resources.ApplyResources(this.Status, "Status");
+            this.Status.BackColor = System.Drawing.Color.White;
+            this.Status.Name = "Status";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Name = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Wait
+            // 
+            this.Wait.Interval = 20;
+            // 
+            // Exit
+            // 
+            resources.ApplyResources(this.Exit, "Exit");
+            this.Exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Exit.ForeColor = System.Drawing.Color.Red;
+            this.Exit.Name = "Exit";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Name = "label4";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.ForeColor = System.Drawing.Color.Yellow;
+            this.label6.Name = "label6";
+            // 
+            // IntervalCounter
+            // 
+            this.IntervalCounter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.IntervalCounter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.IntervalCounter.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.IntervalCounter.ForeColor = System.Drawing.Color.White;
+            this.IntervalCounter.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            resources.ApplyResources(this.IntervalCounter, "IntervalCounter");
+            this.IntervalCounter.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.IntervalCounter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.IntervalCounter.Name = "IntervalCounter";
+            this.IntervalCounter.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.IntervalCounter.ValueChanged += new System.EventHandler(this.IntervalCounter_ValueChanged);
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Name = "label7";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.ForeColor = System.Drawing.Color.Silver;
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(31)))));
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(31)))));
+            this.panel2.Controls.Add(this.panel4);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(31)))));
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Name = "panel3";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(31)))));
+            resources.ApplyResources(this.panel4, "panel4");
+            this.panel4.Name = "panel4";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(31)))));
+            resources.ApplyResources(this.panel5, "panel5");
+            this.panel5.Name = "panel5";
+            // 
+            // panel6
+            // 
+            resources.ApplyResources(this.panel6, "panel6");
+            this.panel6.Name = "panel6";
+            // 
+            // Spammer
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.CausesValidation = false;
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.IntervalCounter);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.HyperLink);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.spamBox);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.MinimizeBox = false;
+            this.Name = "Spammer";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SpamCity_FormClosed);
+            this.Load += new System.EventHandler(this.SpamCity_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Spammer_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.IntervalCounter)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox spamBox;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel HyperLink;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer Wait;
+        private System.Windows.Forms.Label Exit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown IntervalCounter;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Threading;
+using Hotkeys;
+
+namespace R6S_Chat_Spammer
+{
+    public partial class Spammer : Form
+    {
+        private Hotkeys.GlobalHotkey ghk1;
+        private Hotkeys.GlobalHotkey ghk2;
+
+        private Keys GetKey(IntPtr LParam)
+        {
+            return (Keys)(LParam.ToInt32() >> 16);
+        }
+
+        public Spammer()
+        {
+            InitializeComponent();
+            ghk1 = new Hotkeys.GlobalHotkey(Constants.NOMOD, Keys.F8, this);
+            ghk1.Register();
+            ghk2 = new Hotkeys.GlobalHotkey(Constants.NOMOD, Keys.F9, this);
+            ghk2.Register();
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Spammer_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void HandleHotkey1()
+        {
+            Timer.Enabled = true;
+        }
+
+        private void HandleHotkey2()
+        {
+            Timer.Enabled = false;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID)
+                switch (GetKey(m.LParam))
+                {
+                    case Keys.F8:
+                        HandleHotkey1();
+                        break;
+                    case Keys.F9:
+                        HandleHotkey2();
+                        break;
+                }
+            base.WndProc(ref m);
+        }
+
+        private bool isRunning = true;
+
+        private void SpamCity_Load(object sender, EventArgs e)
+        {
+            // Attached to the key press status
+            Thread TH = new Thread(Keyboardd);
+            TH.SetApartmentState(ApartmentState.STA);
+            CheckForIllegalCrossThreadCalls = false;
+            TH.Start();
+        }
+
+        // Check the key you press status
+        void Keyboardd()
+        {
+            while (isRunning)
+            {
+                Thread.Sleep(40);
+                if ((Keyboard.GetKeyStates(Key.F8) & KeyStates.Down) > 0)
+                {
+                    
+                }
+                if ((Keyboard.GetKeyStates(Key.F9) & KeyStates.Down) > 0)
+                {
+                    
+                }
+            }
+        }
+        
+        // Messages from the chat box that will be sent to in-game
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+            SendKeys.Send(spamBox.Text);
+            SendKeys.Send("{Enter}");
+        }
+        // Timer rate section
+        private void IntervalCounter_ValueChanged(object sender, EventArgs e)
+        {
+            Timer.Interval = Convert.ToInt16(IntervalCounter.Value);
+        }
+
+        // Creater youtube link
+        private void HyperLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/kzXNCFzvAS");
+        }
+        // Safely exits out of program
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        // Stops the while loop and unregister hotkeys when program is closed for key press status
+        private void SpamCity_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ghk1.Unregiser();
+            ghk2.Unregiser();
+            isRunning = false;
+        }
+
+        private void SetInitial_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetFinal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+    }
+}
+
